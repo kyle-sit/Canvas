b02ad35639335a0cc15d03e39592504329e7944d
Kyle Sit <ktsit@ucsd.edu>
2017-08-15 19:18:08 -0700

    Pan gesture recognizer is implemented in full

--- Canvas/CanvasViewController.swift
+++ Canvas/CanvasViewController.swift
@@ -9,16 +9,53 @@
 import UIKit
 
 class CanvasViewController: UIViewController {
+    
+    //outlets
+    @IBOutlet weak var trayView: UIView!
 
+    //instance vars
+    var trayOriginalCenter: CGPoint!
+    var trayDownOffset: CGFloat!
+    var trayUp: CGPoint!
+    var trayDown: CGPoint!
+    
+    
+    //viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        //setting initial tray positions
+        trayDownOffset = 200
+        trayUp = trayView.center
+        trayDown = CGPoint(x: trayView.center.x ,y: trayView.center.y + trayDownOffset)
     }
 
+    
+    //Memory warning
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    //action outlet for gesture recognizer on the tray view
+    @IBAction func didPanTray(_ sender: UIPanGestureRecognizer) {
+        //translation and velocity parameter
+        let translation = sender.translation(in: view)
+        let velocity = sender.velocity(in: view)
+        
+        if sender.state == .began {
+            trayOriginalCenter = trayView.center
+        } else if sender.state == .changed {
+            trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
+        } else if sender.state == .ended {
+            if velocity.y > 0 {
+                trayView.center = trayDown
+            }
+            else {
+                trayView.center = trayUp
+            }
+        }
     }
     
 
